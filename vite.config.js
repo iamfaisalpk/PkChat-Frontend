d8bc9d01a01e39c73bde1ee943dfe888b091389e
iamfaisalpk <iamfaizy123@gmail.com>
2026-04-02 17:41:01 +0530

    ss

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from "path";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/WhatsApp-Frontend/" : "/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
